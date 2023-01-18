32815c006d7cde9320265e525fec1ec9fe16871b
Thomas Lievre <lievre.thomas@gmail.com>
2023-01-18 15:09:10 +0100

    fix inherit model

--- models/model.py
+++ models/model.py
@@ -1,12 +1,6 @@
-from logging import warning
-from sklearn.model_selection import GridSearchCV
 import pandas as pd
-from abc import ABC
+from abc import ABC, abstractmethod
 
-# Will be raised when model affect in parameter don't inherit from
-# our models template
-class ErrorMethod(Exception):
-    pass
 
 class Model(ABC):
 
@@ -19,24 +13,14 @@ class Model(ABC):
         if not isinstance(X_test, pd.DataFrame):
             raise ValueError("X_test must be pandas.Dataframe")
 
-        self.X_train = X_train
-        self.y_train = y_train
-        self.X_test = X_test
-        
-        self.hparam = {}
+        self.__X_train = X_train
+        self.__y_train = y_train
+        self.__X_test = X_test
 
-        self.model = 0
+    @abstractmethod
+    def __fit_cv(self, nb_fold = 10):
+        pass
 
-        self.y_pred = None
-
-    def fit_CV(self, nb_fold=10):
-        
-        if isinstance(self.model, int):
-            raise ErrorMethod("fit_CV must be used on object from inherited class from module greenml.models")
-        else:
-
-            grid = GridSearchCV(self.model, self.hparam, cv=nb_fold, verbose=True)
-            grid.fit(self.X_train, self.y_train)
-            self.y_pred = grid.best_estimator_.predict(self.X_test)
-
-            return self.y_pred
+    @abstractmethod
+    def predict(self, nb_fold = 10):
+        pass
--- models/svm_linear.py
+++ models/svm_linear.py
@@ -1,17 +1,23 @@
 import numpy as np
-from greenml.runners.models import Model
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import GridSearchCV
+
+from models import Model
 
 class SVM_Linear(Model):
 
-    def __init__(self, X_train, y_train, X_test):
+    # params need to be test
+    def __init__(self, X_train, y_train, X_test, params = {'tol': np.linspace(1e-5, 1e-2, 5), 'C': np.linspace(1e-3, 1, 5)}):
 
         super().__init__(X_train, y_train, X_test)
 
-        from sklearn.svm import LinearSVC
+        self.__hparam = params
 
-        self.method = LinearSVC()
+    def __fit_cv(self, nb_fold = 10):
+        grid = GridSearchCV(LinearSVC(), self.__hparam, cv = nb_fold, verbose = True)
+        grid.fit(self.__X_train, self.__y_train)
+        return grid
 
-        self.hparam = {
-            "tol": np.linspace(1e-5, 1e-2, 5),
-            "C": np.linspace(1e-3, 1, 5)
-        }
+    def predict(self, nb_fold = 10):
+        grid = self.__fit_cv(nb_fold)
+        return grid.best_estimator_.predict(self.__X_test)
