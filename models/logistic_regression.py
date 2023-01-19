d0f0a96888ca403230afcd0ad2e36726a4ccc1a1
Thomas Lievre <lievre.thomas@gmail.com>
2023-01-19 15:43:55 +0100

    fix logisticRegression default parameters

--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -2,15 +2,20 @@ from greenml.models.model import Model
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
 
+import numpy as np
 
 class LogisticRegression(Model):
 
-    def __init__(self, X_train, y_train, X_test, nb_folds, params):
-
+    def __init__(self, X_train, y_train, X_test, nb_folds,
+        params = {
+            'tol': np.linspace(1e-5, 1e-2, 5),
+            'C': np.linspace(1e-3, 1, 5),
+            'penalty' : ['None', 'l2', 'l1', 'elasticnet'],
+            'solver' : ['lbfgs', 'liblinear', 'newton-cg', 'newton-cholesky', 'sag', 'saga']}
+            # The choice of the algorithm depends on the penalty chosen. Supported penalties by solve
+        ):
         super().__init__(X_train, y_train, X_test, nb_folds)
-
         self._hparam = params
-
 
     def _fit_cv(self):
         grid = GridSearchCV(LogisticRegression(), self._hparam, cv = self._nb_folds, verbose = True)
