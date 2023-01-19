e9a9b034e52d559f4d08721142e07743da841783
Thomas Lievre <lievre.thomas@gmail.com>
2023-01-19 11:14:38 +0100

    add LogisticRegression class

---
+++ models/logistic_regression.py
@@ -0,0 +1,21 @@
+from greenml.models.model import Model
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+
+
+class LogisticRegression(Model):
+
+    def __init__(self, X_train, y_train, X_test, nb_folds, params):
+
+        super().__init__(X_train, y_train, X_test, nb_folds)
+
+        self._hparam = params
+
+    def _fit_cv(self):
+        grid = GridSearchCV(LogisticRegression(), self._hparam, cv = self._nb_folds, verbose = True)
+        grid.fit(self._X_train, self._y_train)
+        return grid
+
+    def predict(self):
+        grid = self._fit_cv()
+        return grid.best_estimator_.predict(self._X_test)
