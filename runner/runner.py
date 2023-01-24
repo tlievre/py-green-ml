b35416e581f5f901d07be3ec5c94108fe0117e4d
Thomas Lievre <lievre.thomas@gmail.com>
2023-01-24 17:51:35 +0100

    refactor : create the main script runner.py

---
+++ runner/runner.py
@@ -0,0 +1,90 @@
+# coding: utf-8
+
+import json
+import pandas as pd
+import importlib
+
+from exception import Exception
+
+from measure import PyRaplMeasurement, CodeCarbonMeasurement
+
+class Runner() :
+    """This abstract class implement the basic structure of our machine
+    learning statistic methods (classification, regression, clustering ...).
+    """
+    
+    def __init__(self, config) :
+        """
+        Args:
+            config (dict): contain all parameters of a ml_method object.
+            the dictionnary should be organized like below :
+                - name : str
+                - path : str
+                - task : str
+                - y : array
+                - token : str
+                - folds : int
+            model_path (str): path of the machine learning model.
+        """
+        # config load
+        # /!\ types should be test
+        self._data_name = config["name"]
+        self._data_path = config["path"]
+        self._data_y_var = config["y"]
+        self._nb_folds = config["folds"]
+
+        # get the measure method
+        if (measure := config["measure"]) == None:
+            self._measure = None 
+        elif measure == "pyRapl":
+            self._measure = PyRaplMeasurement()
+        elif measure == "codecarbon":
+            self._measure = CodeCarbonMeasurement()
+        else:
+            raise Exception("measure value does't refer an existing \
+                measurement method")
+        
+        # data load /!\ à modifier dans REDIS
+        try:
+            data_train = pd.read_csv(str(self._data_path) + str(self._data_name) \
+                + "_train.csv")
+            data_test = pd.read_csv(str(self._data_path) + str(self._data_name) \
+            + "_test.csv")
+        except FileNotFoundError:
+            print("File not found.")
+        except pd.errors.EmptyDataError:
+            print("No data")
+        except pd.errors.ParserError:
+            print("Parse error")
+        except Exception:
+            print("Some other exception")
+
+        # fetch suitable task and token
+        # task and token to test
+        task = config["task"]
+        token = config["mod_token"]
+        with open("greenml/models.json") as f:
+            token_list = json.load(f)
+        tok = token_list[task][token]
+        tok2 = tok.lower()
+
+        # get the suitable method class
+        try :
+            method_path = "greenml.runner.methods." + task
+            method = importlib.import_module(method_path)
+            constr_method = getattr(method, token)
+            self._method = constr_method(data_train, data_test, tok2, config["folds"])
+        except (ImportError, AttributeError):
+            raise ValueError("Unknown " + method_path)
+
+    
+    def run(self) :
+        """This protected method aim to call the suitable model.
+
+        Returns:
+            array : 1-D vector of the predicted value
+        """
+        # train the model
+        self._method.fit()
+        # get the metrics
+        self._method.get_metrics()
