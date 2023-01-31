9b04f39abbe48fd1ec87b2cb6ff4ee97d799462c
Thomas Lievre <lievre.thomas@gmail.com>
2023-01-31 11:12:13 +0100

    update cardinalities in runner/methods/methods.py

--- runner/methods/method.py
+++ runner/methods/method.py
@@ -8,18 +8,15 @@ class Method(ABC) :
     learning statistic methods (classification, regression, clustering ...).
     """
     
-    def __init__(self, data_train, data_test, data_y_var, token, nb_folds) :
+    def __init__(self, data_train, data_test, data_y_var, models_name, nb_folds) :
         """
 
-        Args:
-            data_train (_type_): _description_
-            data_test (_type_): _description_
-            data_y_var (_type_): _description_
-            token (_type_): _description_
-            nb_folds (_type_): _description_
-
-        Raises:
-            ValueError: _description_
+        Attributes:
+            data_train (obj): Train data, store in a pandas.DataFrame object.
+            data_test (obj): test data, store in a pandas.DataFrame object.
+            data_y_var (str): Column name of the response variable.
+            models_name (list): List of models (similar classes) names.
+            nb_folds (int): Folds numbers. 
         """
 
         # train/test split
@@ -27,18 +24,19 @@ class Method(ABC) :
         self._y_train = data_train[data_y_var]
         self._X_test = data_test.loc[:, data_test.columns != data_y_var]
         self._y_test = data_test[data_y_var]
-
         self._nb_folds = nb_folds
-
-        tok2 = token.lower()
-        # get the suitable method class
-        try :
-            module_path = "greenml.runner.methods.models." + tok2
-            model = importlib.import_module(module_path)
-            constr_model = getattr(model, token)
-            self._model = constr_model(self._X_train, self._y_train, self._X_test, self._nb_folds)
-        except (ImportError, AttributeError):
-            raise ValueError("Unknown " + module_path)
+        
+        # import
+        for model_name in models_name:
+            module_name = model_name.lower()
+            # get the suitable method class
+            try :
+                module_path = "greenml.runner.methods.models." + module_name
+                module = importlib.import_module(module_path)
+                constr_model = getattr(module, model_name)
+                self._model = constr_model(self._X_train, self._y_train, self._X_test, self._nb_folds)
+            except (ImportError, AttributeError):
+                raise ValueError("Unknown " + module_path)
 
     
     def fit(self) :
