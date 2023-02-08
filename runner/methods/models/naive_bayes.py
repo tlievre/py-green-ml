aa5ab9e0426796984ad44a2682df04d819c8eaa3
manceaur <manceaux.renan@laposte.net>
2023-02-08 13:40:16 +0100

    fix: naive bayes

--- runner/methods/models/naive_bayes.py
+++ runner/methods/models/naive_bayes.py
@@ -1,16 +1,15 @@
 from greenml.runner.methods.models.model import Model
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import GridSearchCV
 
 # /!\ doesn't work, need to be fixed
 class Naive_Bayes(Model):
-    """Naives Bayes class. It uses the MultinomialNB from sklearn.naive_bayes.
+    """Naives Bayes class. It uses the GaussianNB from sklearn.naive_bayes.
     Inherit from the abstract class model
     """
 
     def __init__(self, X_train, y_train, X_test, nb_folds, consumption_method,
-        params = {'alpha': [0.00001, 0.0001, 0.001, 0.1, 1, 10, 100, 1000],
-                  'fit_prior':[True,False]}):
+        params = {'var_smoothing': [1e-9, 1e-7, 1e-5, 1e-3, 1e-1]}):
         """_summary_
 
         Args:
@@ -19,18 +18,18 @@ class Naive_Bayes(Model):
             X_test (pd.DataFrame): Test set predictors.
             nb_folds (int): Folds numbers used in cross validation.
             params (dict, optional): Contains listes of tuning parameters given
-            by sklearn MultinomialNB() model. Defaults to 
-                {'alpha': [0.00001, 0.0001, 0.001, 0.1, 1, 10, 100, 1000],'fit_prior':[True,False]}.
+            by sklearn GaussianNB() model. Defaults to 
+                {'var_smoothing': [1e-9, 1e-7, 1e-5, 1e-3, 1e-1]}.
         """
         super().__init__(X_train, y_train, X_test, nb_folds,
             consumption_method)
         self._parameters = params
-        self.__grid = GridSearchCV(MultinomialNB(), params,
+        self.__grid = GridSearchCV(GaussianNB(), params,
             cv = self._nb_folds, verbose = True)
     
 
     def fit_cv(self):
-        """Compute the predicted response vector given by the trained model MultinomialNB.
+        """Compute the predicted response vector given by the trained model GaussianNB.
 
         Returns:
             float: measure.
@@ -47,7 +46,7 @@ class Naive_Bayes(Model):
         return return_value
 
     def predict(self):
-        """Compute the predicted response vector given sklearn trained model MultinomialNB.
+        """Compute the predicted response vector given sklearn trained model GaussianNB.
 
         Returns:
             array: 1-D predicted response vector.
